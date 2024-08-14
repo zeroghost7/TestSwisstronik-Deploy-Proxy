982307db2704e950687c39c7da67065799f95677
Pido Haryanto <tengikman579@gmail.com>
2024-08-14 16:39:39 +0000

    update

--- utils/deployed-address.ts
+++ utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xea65D3f80d2253293a64e2f8DdF4CEC401823053'
-export const ProxyAdmin = '0x6a0baE5CF96d14e39FA9F5AdB4fadABb55B22E17'
-export const SWTRImplementation = '0xE4e93a474734E22A4f660EF1fE7aC3fc096E856c'
+export const SWTRProxy = '0x3FBFD81fE9C9A7054F99CD69b3f9aE93A658f57F'
+export const ProxyAdmin = '0x4373A980519aFda29a947859a1c9d440568c20b7'
+export const SWTRImplementation = '0x1878b3dBAb54A159112cfDf2eebBe18e79baBa21'
     
